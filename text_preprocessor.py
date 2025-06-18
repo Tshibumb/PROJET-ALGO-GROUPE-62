96be346a7e382a67ee38cca74ee49b15b98e7cb2
TSHIBUMB-A-MBAZ <gradytshibumb531@gmail.com>
2025-06-18 16:17:30 +0100

    Update text_preprocessor.py

--- text_preprocessor.py
+++ text_preprocessor.py
@@ -1,1 +1,15 @@
+import re
 
+def nettoyer_ligne(ligne, ignorer_casse=True, supprimer_ponctuation=True):
+    if ignorer_casse:
+        ligne = ligne.lower()
+    if supprimer_ponctuation:
+        ligne = re.sub(r'[\W_]+', ' ', ligne)
+    ligne = ligne.strip()
+    return ligne
+
+def pretraiter_texte(lignes, mode_strict=False):
+    if mode_strict:
+        return [ligne.strip() for ligne in lignes]
+    else:
+        return [nettoyer_ligne(ligne) for ligne in lignes]
